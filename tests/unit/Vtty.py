fc53694431aa8bd7239fd7cdfbf6578cb9041be0
Travis Brown <travisb@travisbrown.ca>
2013-12-11 05:33:21 +0000

    Add the first dogfooding test.
    git-svn-id: svn+ssh://svnrepo/projects/lousy/trunk@7075 561c3618-0fe6-0310-b5d6-fc65c73fee30

---
+++ tests/unit/Vtty.py
@@ -0,0 +1,34 @@
+# Tests of the various emulated terminal classes
+
+import lousy
+
+class TerminalTestCase(lousy.TestCase):
+	def assertCellChar(self, cell, char):
+		self.assertIsNotNone(cell)
+		self.assertEqual(cell.char, char)
+
+class EmulatedTerminalTests(TerminalTestCase):
+	''' Test the EmulatedTerminal class '''
+	def setUp1(self):
+		self.vty = lousy.EmulatedTerminal()
+
+	def tearDown1(self):
+		pass
+
+	def test_basicEcho(self):
+		self.vty.interpret('a')
+		cell = self.vty.getCell(0, 0)
+		self.assertCellChar(cell, 'a')
+
+		self.vty.interpret('s')
+		cell = self.vty.getCell(0, 1)
+		self.assertCellChar(cell, 's')
+
+		self.vty.interpret('d')
+		cell = self.vty.getCell(0, 2)
+		self.assertCellChar(cell, 'd')
+
+		self.vty.interpret('f')
+		cell = self.vty.getCell(0, 3)
+		self.assertCellChar(cell, 'f')
+
